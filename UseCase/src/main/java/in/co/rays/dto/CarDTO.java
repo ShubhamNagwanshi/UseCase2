544f29dff0ec99aef42334319345ff6d1c1cecf1
ShubhamNagwanshi <shubhamnagwanshi767@gmail.com>
2025-02-15 23:21:57 +0530

    second branch

---
+++ UseCase/src/main/java/in/co/rays/dto/CarDTO.java
@@ -0,0 +1,76 @@
+package in.co.rays.dto;
+
+import java.util.Date;
+
+public class CarDTO {
+
+	private long serialNumber;
+	private String modelName;
+	private int modelNumber;
+	private String brandName;
+	private String ownerName;
+	private Date purchaseDate;
+	private Date deliveryDate;
+	private String dealerName;
+	private String remark;
+	
+	public long getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(long serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public String getModelName() {
+		return modelName;
+	}
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	public int getModelNumber() {
+		return modelNumber;
+	}
+	public void setModelNumber(int modelNumber) {
+		this.modelNumber = modelNumber;
+	}
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+	public String getDealerName() {
+		return dealerName;
+	}
+	public void setDealerName(String dealerName) {
+		this.dealerName = dealerName;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+	
+	
+	
+}
